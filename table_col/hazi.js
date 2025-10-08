21878f3d869584851a9108122e94e8aad7e09854
Prebor <prebor.jonathan_202@bolyai-szakkozep.hu>
2025-10-08 17:14:19 +0200

    hf

---
+++ table_col/hazi.js
@@ -0,0 +1,86 @@
+/**
+ * @type{{szerzonev:string, korszak:string, szereto1:string, szereto2:string}[]}
+ */
+
+const tomb = [
+    {
+        szerzonev: 'Balassi Bálint',
+        korszak: 'reformáció',
+        szereto1: 'Losonczy Anna',
+        szereto2: 'Dobó Krisztina',
+    },
+
+    {
+        szerzonev: 'Csokonai Vitéz Mihály',
+        korszak: 'felvilágosodás',
+        szereto1: 'Vajda Juliána',
+    },
+
+    {
+        szerzonev: 'Petőfi Sándor',
+        korszak: 'magyar romantika',
+        szereto1: 'Mednyánszky Berta',
+        szereto2: 'Szendrey Júlia',
+    },
+
+    {
+        szerzonev: 'Ady Endre',
+        korszak: '20. század',
+        szereto1: 'Léda',
+        szereto2: 'Csinszka',
+    }
+]
+
+const table = document.createElement('table');
+document.body.appendChild(table)
+
+const thead = document.createElement('thead')
+table.appendChild(thead)
+
+const tbody = document.createElement('tbody')
+table.appendChild(tbody)
+
+const tr = document.createElement('tr')
+thead.appendChild(tr)
+
+const th1 = document.createElement('th')
+tr.appendChild(th1)
+
+const th2 = document.createElement('th')
+tr.appendChild(th2)
+
+const th3 = document.createElement('th')
+tr.appendChild(th3)
+
+th1.innerText = "Szerző neve"
+th2.innerText = "Korszak"
+th3.innerText = "Szerelmek"
+th3.colSpan = "2"
+
+for (const col of tomb){
+    
+    const tr1 = document.createElement('tr')
+    tbody.appendChild(tr1)
+
+    const td1 = document.createElement("td")
+    tr1.appendChild(td1)
+    td1.innerText = col.szerzonev
+
+    const td2 = document.createElement("td")
+    tr1.appendChild(td2)
+    td2.innerText = col.korszak
+
+    const td3 = document.createElement("td")
+    tr1.appendChild(td3)
+    td3.innerText = col.szereto1
+
+    if (col.szereto2 != undefined){ // ha van masodik szereto, akkor hozzuk letre a td-t (ez azt jelenti hogy kb mindig)
+        const td4 = document.createElement("td")
+        tr1.appendChild(td4)
+        td4.innerText = col.szereto2
+        
+    }
+    else {
+        td3.colSpan = "2" // ha nincs masodik szereto, akkor a harmadik td legyen 2 oszlopot egyesito
+    }
+}
--- table_col/kitero.js
+++ table_col/kitero.js
@@ -1,37 +1,59 @@
-console.log("Hello World")
-let valtozo1 = "Random szöveg"
-/** 
- * @type{string}
-*/
-const valtozo2 = "Konstans szöveg" 
 
-/** 
- * @type{string[]}
-*/
-const arr = ["alma", "valami", "text"]
-arr[0]
-arr[1]
-arr[2]
+    console.log("Hello World")
+    let valtozo1 = "Random szöveg"
 
-/*
-for (let i = 0; arr.length; i++){
-    console.log(arr[i])
-}
-*/
+    /** 
+     * @type{string}
+    */
+    const valtozo2 = "Konstans szöveg" 
 
-/*
-for (const key in b){
-    console.log(`${hey}:${b[key]}`)
-}
-*/
+    /** 
+     * @type{string[]}
+    */
+    const arr = ["alma", "valami", "text"]
+    arr[0]
+    arr[1]
+    arr[2]
+    // ezeket nem irja ki mert "consol.log" nincs
 
-/**
- * @type{{nev:string, age:number}}
- */
-const a ={
-    nev:'gomszab',  
-    age: 24
+    for (let i = 0; i < arr.length; i++)  //ez a sima for ciklus, vegigmegyunk az egesz "arr" tombon
+    { 
+            console.log(arr[i]); 
+    };
 
-    /*nev es age = key; "gomszab" es 24 = value*/ 
-}
-console.log(a)
+    for (const index in arr)  // "for in" ciklus: végigmegy egy objektum nevein vagy egy tomb sorszamain (mert in = valami-ben, azaz pl. ciklus-BAN)
+    {
+        console.log(index, arr[index]);
+    };
+
+    for (const value of arr) // a ciklus vegigmegy egy tomb ertekein (pl. az arr-nak az ertekei: ["alma", "valami", "text"] )
+    { 
+        console.log(value); 
+    };
+
+    /**
+     * @type{{nev:string, age:number}}
+     */
+    const a ={
+        nev:'gomszab',  
+        age: 24
+
+        /*nev es age = key; "gomszab" es 24 = value*/ 
+    }
+    console.log(a)
+
+    arr.push("egy negyedik ertek!!!");
+    console.log(arr);
+
+    /** 
+    * @type{{name: {string}, age: {number}[]}
+    **/ 
+    const people = [ 
+        { name: "Sandor", age: 24 }, 
+        { name: "Bela", age: 21 }, 
+        { name: "Pista", age: 30 }, ];
+
+    for (const person of people)
+    { 
+        console.log(person.name); 
+    }
