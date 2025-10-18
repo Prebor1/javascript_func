8c6977fad3dbe0f45d955df1f84e05da995f332c
Prebor1 <prebor.jonathan_2022b@bolyai-szakkozep.edu>
2025-10-18 09:14:09 +0200

    10.18. ora

--- table_col/hazi.js
+++ table_col/hazi.js
@@ -43,45 +43,61 @@ table.appendChild(tbody)
 const tr = document.createElement('tr')
 thead.appendChild(tr)
 
-const th1 = document.createElement('th')
-tr.appendChild(th1)
 
-const th2 = document.createElement('th')
-tr.appendChild(th2)
+// const th1 = document.createElement('th')
+// tr.appendChild(th1)
 
-const th3 = document.createElement('th')
-tr.appendChild(th3)
+// const th2 = document.createElement('th')
+// tr.appendChild(th2)
 
+// const th3 = document.createElement('th')
+// tr.appendChild(th3)
+
+
+createCellElement("th", "Szerző neve", tr)
+createCellElement("th", "Korszak", tr)
+const c = createCellElement("th", "Szerelmek", tr)
+c.colSpan = 2
+
+/*
 th1.innerText = "Szerző neve"
 th2.innerText = "Korszak"
 th3.innerText = "Szerelmek"
 th3.colSpan = "2"
+*/
 
 for (const col of tomb){
     
     const tr1 = document.createElement('tr')
     tbody.appendChild(tr1)
 
-    const td1 = document.createElement("td")
-    tr1.appendChild(td1)
-    td1.innerText = col.szerzonev
+    // const td1 = document.createElement("td")
+    // tr1.appendChild(td1)
+    // td1.innerText = col.szerzonev
 
-    const td2 = document.createElement("td")
-    tr1.appendChild(td2)
-    td2.innerText = col.korszak
+    // const td2 = document.createElement("td")
+    // tr1.appendChild(td2)
+    // td2.innerText = col.korszak
 
-    const td3 = document.createElement("td")
-    tr1.appendChild(td3)
-    td3.innerText = col.szereto1
+    // const td3 = document.createElement("td")
+    // tr1.appendChild(td3)
+    // td3.innerText = col.szereto1
+
+createCellElement("td", col.szerzonev, tr1)
+createCellElement("td", col.korszak, tr1)
+const colTd = createCellElement("td", col.szereto1, tr1)
 
     if (col.szereto2 != undefined){ // ha van masodik szereto, akkor hozzuk letre a td-t (ez azt jelenti hogy kb mindig)
-        const td4 = document.createElement("td")
-        tr1.appendChild(td4)
-        td4.innerText = col.szereto2
+        
+        // const td4 = document.createElement("td")
+        // tr1.appendChild(td4)
+        // td4.innerText = col.szereto2
+
+        createCellElement("td", col.szereto2, tr1)
         
     }
     else {
-        td3.colSpan = "2" // ha nincs masodik szereto, akkor a harmadik td legyen 2 oszlopot egyesito
+        colTd.colSpan = "2" // ha nincs masodik szereto, akkor a harmadik td legyen 2 oszlopot egyesito
     }
 }
 
@@ -90,10 +106,12 @@ for (const col of tomb){
  * @param {string} cellType //th vagy td lesz-e létrehozva?
  * @param {string} cellContent //cella tartalma
  * @param {HTMLTableRowElement} cellRow //melyik sorhoz csatolunk
+ * @returns {HTMLTableCellElement} // a létrehozott cella visszatérése (?)
  */
 function createCellElement(cellType, cellContent, cellRow) //fuggveny letrehoz egy tablazatcella elemet
 {
     const a = document.createElement(cellType);
     a.innerText = cellContent;
     cellRow.appendChild(a);
+    return a;
 }
